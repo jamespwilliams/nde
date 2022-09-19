2992afa2b1207f60aad3c3644dd5f15f4716fedd
James Williams <jamesphwilliams@gmail.com>
2022-09-19 16:58:06 +0100

    Fix lua warning

--- base-neovim-config.lua
+++ base-neovim-config.lua
@@ -24,7 +24,7 @@ vim.api.nvim_set_keymap('n', '<space>q',
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
