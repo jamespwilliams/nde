74d29d8b5aa6579da8a47ebb4552db18f9662b3a
James Williams <jamesphwilliams@gmail.com>
2023-07-09 21:58:28 +0100

    Refactor away shell.nix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,79 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in { devShell = import ./shell.nix { inherit pkgs; }; });
+      let
+
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        vimrc = ''
+          source ${pkgs.fzf.out}/share/vim-plugins/fzf/plugin/fzf.vim
+
+          lua << EOF
+          ${builtins.readFile ./base-neovim-config.lua}
+          EOF
+
+          ${builtins.readFile ./extra-neovim-config.vim};
+        '';
+
+        # bat.vim syntax highlighting:
+        bat-vim = pkgs.vimUtils.buildVimPlugin {
+          name = "bat.vim";
+          src = pkgs.fetchFromGitHub {
+            owner = "jamespwilliams";
+            repo = "bat.vim";
+            rev = "e2319b07ed6e74cdd70df2be6e8bf066377e22f7";
+            sha256 = "0bmlvziha1crk7x7p1yzdsb55bvpsj434sc28r7xspin9kfnd6y9";
+          };
+        };
+
+        overriden-neovim = pkgs.neovim.override {
+          configure = {
+            customRC = vimrc;
+            packages.packages = with pkgs.vimPlugins; {
+              start = [
+                bat-vim
+                fzf-vim
+                nvim-lspconfig
+                (nvim-treesitter.withPlugins
+                  (plugins: pkgs.tree-sitter.allGrammars))
+                rust-tools-nvim
+                sensible
+              ];
+            };
+          };
+        };
+
+      in with pkgs; {
+        devShell = mkShell {
+          nativeBuildInputs = [
+            fzf
+            go_1_19
+            gopls
+            nodePackages.vim-language-server
+            overriden-neovim
+            ripgrep
+            rnix-lsp
+            sumneko-lua-language-server
+            tmux
+
+            python3
+            pyright
+
+            cargo
+            rustc
+            rustfmt
+            rust-analyzer
+
+            perl
+            perlPackages.PLS
+
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+
+            terraform
+            terraform-ls
+          ];
+        };
+
+      });
 }
